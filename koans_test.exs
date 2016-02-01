527add3e2cd07daba9b2046b606f23aec0445396
Gearnode <bfrimin@student.42.fr>
2016-01-31 17:10:20 -0800

    Add koans for concatenation of two string

--- koans_test.exs
+++ koans_test.exs
@@ -4,4 +4,10 @@ ExUnit.configure trace: true
 defmodule KoansTest do
   # Run unit test on concurrently with other tests
   use ExUnit.Case, async: true
+
+  test "concatenation of two string" do
+    say_hello = fn (name) -> "hello " <> name end
+
+    assert say_hello.("world") == "hello world"
+  end
 end
