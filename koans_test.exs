96b89a140f1e03524d467566d9cbe8aa6a356452
Gearnode <bfrimin@student.42.fr>
2016-01-31 18:00:42 -0800

    Add koans match operators

--- koans_test.exs
+++ koans_test.exs
@@ -29,4 +29,42 @@ defmodule KoansTest do
     assert is_in_range.(1..5, 5) == true
     assert is_in_range.(1..5, 6) == false
   end
+
+  test "match on tuples" do
+    {a, b, c} = {1, "world", :atom}
+
+    assert a == 1
+    assert b == "world"
+    assert c == :atom
+  end
+
+  test "match on list" do
+    [a, b, c] = [1, 2, "hello"]
+
+    assert a == 1
+    assert b == 2
+    assert c == "hello"
+  end
+
+  test "match tuples with atom" do
+    {:ok , a} = {:ok, :valid}
+
+    assert :ok == :ok
+    assert a == :valid
+
+    assert_raise MatchError, fn ->
+      {:ok, result} = {:error, :oops}
+    end
+  end
+
+  test "match with pipe" do
+    [head | tail] = [1, 2, 3, 4, 5]
+
+    assert head == 1
+    assert tail == [2, 3, 4, 5]
+
+    assert_raise MatchError, fn ->
+      [head | tail] = []
+    end
+  end
 end
