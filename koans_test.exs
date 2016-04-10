f2f13342a1018628748d35fe73ea2571cfcc820c
Gearnode <bfrimin@student.42.fr>
2016-04-10 15:42:33 +0200

    Add exercise with comprehensions

--- koans_test.exs
+++ koans_test.exs
@@ -337,6 +337,24 @@ defmodule KoansTest do
     for x <- ~w{"elixir", "ruby", "python"}, into: Map.new, do: { x, String.upcase(x) }
     assert Map.new == %{}
   end
+
+  test "Exercice: ListAndRecursion-8" do
+    tax_rates = [ NC: 0.075, TX: 0.08 ]
+    orders = [
+      [ id: 123, ship_to: :NC, net_amount: 100.00 ],
+      [ id: 124, ship_to: :OK, net_amount:  35.50 ],
+      [ id: 125, ship_to: :TX, net_amount:  24.00 ],
+      [ id: 126, ship_to: :TX, net_amount:  44.80 ],
+      [ id: 127, ship_to: :NC, net_amount:  25.00 ],
+      [ id: 128, ship_to: :MA, net_amount:  10.00 ],
+      [ id: 129, ship_to: :CA, net_amount: 102.00 ],
+      [ id: 130, ship_to: :NC, net_amount:  50.00 ] ]
+
+    res = for order <- orders do
+      total_amount = order[:net_amount] + (tax_rates[order[:ship_to]] || 0)
+      order ++ [ total_amount: total_amount ]
+    end
+  end
   test "concatenation of two string" do
     say_hello = fn (name) -> "hello " <> name end
 
