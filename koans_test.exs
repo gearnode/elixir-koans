3d7ea7f8f4c373ac00a5043851acd3d07681a29d
Gearnode <bfrimin@student.42.fr>
2016-04-10 15:23:57 +0200

    Style

--- koans_test.exs
+++ koans_test.exs
@@ -11,7 +11,7 @@ defmodule KoansTest do
 
     sum = &(&1 + &2)
     assert sum.(2, 5) == 7
-    
+
     pair_tuple_to_list = fn {a, b} -> [a, b] end
     assert pair_tuple_to_list.({1234, 5678}) == [1234, 5678]
   end
